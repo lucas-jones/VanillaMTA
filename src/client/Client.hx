920f15e1ccc4ce19ca74aa9ebd9b3a3c3b20d92d
Lucas Jones <Necxii@gmail.com>
2015-01-08 00:14:09 +0000

    Added camera matrix example

--- src/client/Client.hx
+++ src/client/Client.hx
@@ -1,8 +1,12 @@
 package client;
 
+import mta.Globals;
+import mta.GUI;
+
 class Client 
 {
 	public static function main()
 	{
+		Globals.outputChatBox("Welcome from the client");
 	}
 }
--- src/server/Server.hx
+++ src/server/Server.hx
@@ -2,6 +2,7 @@ package server;
 
 import mta.Globals;
 import mta.Player;
+import mta.math.Vector3;
 
 class Server
 {
@@ -9,6 +10,11 @@ class Server
 	{
 		var player = Player.getRandom();
 
-		Globals.outputChatBox(player.name);
+		player.spawn(2595.31, -1690.17, 1.76);
+		player.fadeCamera(true);
+		player.cameraTarget = player;
+		player.setCameraMatrix(1468.87, -919.25, 100.15, 1468.38, -918.42, 99.88);
+
+		Globals.outputChatBox("Welcome from the server " + player.name);				
 	}
 }
